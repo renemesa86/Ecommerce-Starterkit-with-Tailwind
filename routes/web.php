be06750078392fa918d7f822cade17ee307119ce
René Mesa Martín <renemesa86@gmail.com>
2023-12-09 17:37:13 -0500

    Agregando rutas

--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,12 @@ Route::middleware([
 Route::group(['middleware' => ['auth', 'verified']], function () {
  
     Route::resource('admin/users', \App\Http\Controllers\UserController::class);
+    Route::resource('admin/categorias', \App\Http\Controllers\TagController::class)->parameters([
+        'categorias' => 'category'
+    ]);
+    Route::resource('admin/subcategorias', \App\Http\Controllers\SubcategoryController::class)->parameters([
+        'subcategorias' => 'subcategory'
+    ]);
+    Route::resource('admin/products', \App\Http\Controllers\ProductController::class);
    
 });
